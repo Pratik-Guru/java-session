af8c5a63532fae612396d1b5b3865b0605336107
Pratik-Guru <vpratik.work@gmail.com>
2026-07-24 01:32:16 +0530

    Update welcome message in helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Welcome to Java session for Jenkins CI, 1st change");
+        System.out.println("Welcome to Java session for Jenkins CI, 1st change, 2nd commit running ci");
     }
 }
